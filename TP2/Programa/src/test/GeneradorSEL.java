d833ff194999161aaf2d541d72036bcb98a6050b
gonzalojmartin <gonzalojmartin@hotmail.com>
2015-09-20 13:15:43 -0300

    TP2 entrega final

---
+++ TP2/Programa/src/test/GeneradorSEL.java
@@ -0,0 +1,14 @@
+package test;
+
+import model.SEL;
+
+public class GeneradorSEL {
+
+	public static void main(String[] args) {
+		
+		// Genero Sistemas de Ecuaciones para evaluar el rendimiento
+		// Por "Simple" entendemos coeficientes enteros entre 1 y 11 por ejemplo
+		
+		SEL.generadorAleatorioSEL_Simples(1500);
+	}
+}
